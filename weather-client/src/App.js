825310aca17f099e23b22c6b4d5dc1741eb121bb
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-04-29 23:44:29 -0700

    completed style and added error handling

--- weather-client/src/App.js
+++ weather-client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Weather from './Weather';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Weather />
 
 
--- weather-client/src/Weather.js
+++ weather-client/src/Weather.js
@@ -1,24 +1,30 @@
 import { useState } from 'react'
 import { gql } from '@apollo/client'
 import { client } from './index'
+import './weather.css'
+import TextField from '@material-ui/core/TextField';
+import { Button, CardContent, FormControl, FormHelperText, FormLabel, Input, InputLabel } from '@material-ui/core';
 
 function Weather() {
 
     const [ zip, setZip ] = useState('')
     const [weather, setWeather ] = useState(null)
+    const [unit, setUnit] = useState('imperial')
+    const [status, setStatus] = useState(200)
 
     async function getWeather(){
         try {
             const json = await client.query({
                 query: gql`
                 query {
-                    getWeather(zip:${zip}) {
+                    getWeather(zip:${zip}, units: ${unit}) {
                         temperature
                         description
                         temp_max
                         temp_min
                         feels_like
-                        humidity       
+                        humidity 
+                        cod      
                     }
                 }
                  `
@@ -26,30 +32,68 @@ function Weather() {
             console.log(json)
             setWeather(json)
         } catch(err) {
+            alert(err.message)
             console.log(err.message)
+  
         }
     }
   return (
-    <div className="Weather">
-        <section>
-            {weather ? <h1>Temp: {weather.data.getWeather.temperature}℉</h1> : null}
-            {weather ? <h1>Lows: {weather.data.getWeather.temp_min}℉</h1> : null}
-            {weather ? <h1>Highs: {weather.data.getWeather.temp_max}℉</h1> : null}
-            {weather ? <h1>Feels Like: {weather.data.getWeather.feels_like}℉</h1> : null}
-            {weather ? <h1>Desc: {weather.data.getWeather.description.charAt(0,1).toUpperCase() + weather.data.getWeather.description.slice(1)}</h1> : null}
-            {weather ? <h1>Humidity: {weather.data.getWeather.humidity}</h1> : null}
-        </section>
+
+    <div className="weather">
+        <div className="container">
         <form onSubmit={(e) => {
             e.preventDefault()
             getWeather()
         }}>
-            <input 
+            <Input
+             placeholder='Search Weather...'
             value={zip}
             onChange={(e) => setZip(e.target.value)}
+            required
             />
-            <button type="submit">Submit</button>
+       
+       <Button type="submit">Submit</Button>
+        <FormLabel for='fahrenheit'>Fahrenheit
+            <input 
+              className='radio' 
+              type='radio' 
+              name='fahrenheit' 
+              value='imperial'
+              checked={ unit === "imperial" ? true : false }
+              onChange={e => setUnit(e.target.value)}
+            />
+          </FormLabel>
+
+          <FormLabel for='celcius'>Celcius
+            <input 
+              className='radio' 
+              type='radio' 
+              name='celcius' 
+              value='metric'
+              checked={ unit === "metric" ? true : false }
+              onChange={e => setUnit(e.target.value)}
+            />
+          </FormLabel>
+
+
+
+     
         </form>
+ 
+        <div className="weather_display"> 
+            {weather ? <h1>Temp: {weather.data.getWeather.temperature}</h1> : null}
+            {weather ? <h1>Lows: {weather.data.getWeather.temp_min}</h1> : null}
+            {weather ? <h1>Highs: {weather.data.getWeather.temp_max}</h1> : null}
+            {weather ? <h1>Feels Like: {weather.data.getWeather.feels_like}</h1> : null}
+            {weather ? <h1>Desc: {weather.data.getWeather.description.charAt(0,1).toUpperCase() + weather.data.getWeather.description.slice(1)}</h1> : null}
+            {weather ? <h1>Humidity: {weather.data.getWeather.humidity}</h1> : null}
+        </div>
+
+   
+        </div>
+ 
     </div>
+
   );
 }
 
